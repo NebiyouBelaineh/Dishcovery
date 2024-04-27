8511c1f1c50f1604e579fd6f51be193a5e918e0c
Dridi Chaith <dridi.chaith@gmail.com>
2024-04-27 03:39:44 +0100

    modal

---
+++ dishcovery/static/scripts/bookmarks.js
@@ -0,0 +1,14 @@
+const tags = document.querySelector("#tagsContent");
+const cursor_tag = document.querySelector("#showTags");
+cursor_tag.addEventListener("click", function(e){
+	if(this.children[0].getAttribute("name") === "chevron-up-outline"){
+		this.children[0].setAttribute("name", "chevron-down-outline")
+	}else{
+		this.children[0].setAttribute("name", "chevron-up-outline")
+
+	}
+})
+$("#tagsContent").hide();
+$("#showTags").click(function(){
+	$("#tagsContent").slideToggle(400);
+  });
