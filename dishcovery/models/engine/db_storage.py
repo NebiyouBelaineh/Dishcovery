0922ed77e72efa0d653e169db4dc31f1cc0bd9fb
NebiyouBelaineh <nebiyouseifu2@gmail.com>
2024-10-14 14:55:11 +0300

    Fix bug/Creates the database and tables if they do not exist.

--- dishcovery/models/engine/db_storage.py
+++ dishcovery/models/engine/db_storage.py
@@ -5,7 +5,7 @@ Contains the class DBStorage
 
 from os import environ
 from dishcovery.models.base_model import BaseModel, Base
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, text
 from sqlalchemy.orm import scoped_session, sessionmaker
 from dotenv import load_dotenv
 
@@ -16,20 +16,69 @@ load_dotenv()
 class DBStorage:
     """interaacts with the MySQL database"""
     __engine = None
+    __engine_without_db = None
     __session = None
 
     def __init__(self):
         """Instantiate a DBStorage object"""
         DISHCOVERY_MYSQL_USER = environ.get('DISHCOVERY_MYSQL_USER')
         DISHCOVERY_MYSQL_PWD = environ.get('DISHCOVERY_MYSQL_PWD')
-        DISHCOVERY_MYSQL_HOST = environ.get('DISHCOVERY_MYSQL_HOST')
+        DISHCOVERY_MYSQL_HOST = environ.get('DISHCOVERY_MYSQL_HOST',
+                                            'dishcovery-mysql')
         DISHCOVERY_MYSQL_DB = environ.get('DISHCOVERY_MYSQL_DB')
         DISHCOVERY_ENV = environ.get('DISHCOVERY_ENV')
+        # Create engine without database
+        self.__engine_without_db = create_engine(
+                                                'mysql+mysqldb://{}:{}@{}'
+                                                .format(DISHCOVERY_MYSQL_USER,
+                                                        DISHCOVERY_MYSQL_PWD,
+                                                        DISHCOVERY_MYSQL_HOST))
+        # Create the database if it does not exist
+        try:
+            with self.__engine_without_db.connect() as connection:
+                connection.execute(text(
+                    f"CREATE DATABASE IF NOT EXISTS {DISHCOVERY_MYSQL_DB}"))
+                connection.execute(text(
+                    f"USE {DISHCOVERY_MYSQL_DB}"))
+                connection.execute(text(
+                    f"""CREATE TABLE IF NOT EXISTS users (
+                    id VARCHAR(36) PRIMARY KEY,
+                    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP\
+                    ON UPDATE CURRENT_TIMESTAMP,
+                    email VARCHAR(128) NOT NULL UNIQUE,
+                    password VARCHAR(128) NOT NULL,
+                    firstname VARCHAR(60),
+                    lastname VARCHAR(60)
+                    )"""))
+                connection.execute(text(
+                    f"""CREATE TABLE IF NOT EXISTS bookmarks (
+                    id VARCHAR(36) PRIMARY KEY,
+                    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP\
+                    ON UPDATE CURRENT_TIMESTAMP,
+                    label TEXT NULL,
+                    source VARCHAR(60) NULL,
+                    image_link TEXT NULL,
+                    ingredients TEXT NULL,
+                    total_time FLOAT NULL DEFAULT 0,
+                    calories FLOAT NULL DEFAULT 0.0,
+                    link TEXT NULL,
+                    tags TEXT NULL,
+                    user_id VARCHAR(60) NOT NULL,
+                    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE\
+                    CASCADE
+                    )"""))
+        except Exception as e:
+            print(f"DB Connection not successful: {e}")
+
+        # Connect to the database with the connection url including db name
         self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'.
                                       format(DISHCOVERY_MYSQL_USER,
                                              DISHCOVERY_MYSQL_PWD,
                                              DISHCOVERY_MYSQL_HOST,
                                              DISHCOVERY_MYSQL_DB))
+
         if DISHCOVERY_ENV == "test":
             BaseModel.metadata.drop_all(self.__engine)
 
@@ -70,33 +119,3 @@ class DBStorage:
     def close(self):
         """call remove() method on the private session attribute"""
         self.__session.remove()
-
-    # def get(self, cls, id):
-    #     """
-    #     Returns the object based on the class name and its ID, or
-    #     None if not found
-    #     """
-    #     if cls not in classes.values():
-    #         return None
-
-    #     all_cls = models.storage.all(cls)
-    #     for value in all_cls.values():
-    #         if (value.id == id):
-    #             return value
-
-    #     return None
-
-    # def count(self, cls=None):
-    #     """
-    #     count the number of objects in storage
-    #     """
-    #     all_class = classes.values()
-
-    #     if not cls:
-    #         count = 0
-    #         for clas in all_class:
-    #             count += len(models.storage.all(clas).values())
-    #     else:
-    #         count = len(models.storage.all(cls).values())
-
-    #     return count
